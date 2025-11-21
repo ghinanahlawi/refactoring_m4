9e0459b06a19a48ac556f4ac1e5f0110ce3212cb
ghinanahlawi <ghina_nahlawi@icloud.com>
2025-11-20 19:09:38 -0500

    Task 1.1: fixing all checkstyle

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,39 @@
 package theater;
 
+/**
+ * Class representing a play.
+ *
+ * <p>Contains the play's name and type (e.g., tragedy, comedy).</p>
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
+    /**
+     * Constructor for Play.
+     *
+     * @param name the name of the play, cannot be null
+     * @param type the type of the play, cannot be null
+     */
 
     public Play(String name, String type) {
         this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
